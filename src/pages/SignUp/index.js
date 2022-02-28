0a964d24d665b2736a233adf796ef3820ba65501
Mateus Giroletti <mateusgiroletti97@gmail.com>
2022-02-28 00:51:56 -0300

    Adding pages

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,44 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { Form, Input } from "@rocketseat/unform";
+import * as Yup from "yup";
 
-// import { Container } from './styles';
+import logo from '../../assets/logo.png';
+
+import { signUpRequest } from "../../store/modules/auth/actions";
+
+const schema = Yup.object().shape({
+    name: Yup.string().required("O nome é obrigatório"),
+    email: Yup.string()
+        .email("Insira um e-mail válido")
+        .required("O email é obrigatório"),
+    password: Yup.string()
+        .min(6, "No mínimo 6 caracteres")
+        .required("A senha é obrigatória")
+});
 
 function SignUp() {
-	return <h1>SignUp</h1>;
+    const dispatch = useDispatch();
+
+    function handleSubmit({ name, email, password }) {
+        dispatch(signUpRequest(name, email, password));
+    }
+
+    return (
+        <>
+            <img src={logo} alt='AppService' />
+
+            <Form schema={schema} onSubmit={handleSubmit}>
+                <Input name="name" type="text" placeholder="Nome Completo" />
+                <Input name="email" type="email" placeholder="Seu e-mail" />
+                <Input name="password" type="password" placeholder="Sua senha" />
+
+                <button type='submit'>Criar</button>
+                <Link to="/">Já tenho login</Link>
+            </Form>
+        </>
+    );
 }
 
 export default SignUp;
